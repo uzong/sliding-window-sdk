b3958a8160520dae7144b357dfdb9692e5caf20d
sky <sky.hu@spotterio.com>
2025-07-20 19:03:14 +0800

    去除无效编译类

--- sliding-window-starter/target/classes/META-INF/scripts/calculate_count.lua
+++
@@ -1,4 +0,0 @@
-redis.pcall('zadd', KEYS[1], ARGV[1], ARGV[5]);
-redis.pcall('zremrangebyscore', KEYS[1], 0, ARGV[2]);
-redis.pcall("expire", KEYS[1], ARGV[3]);
-return redis.pcall("zcard",KEYS[1]);
